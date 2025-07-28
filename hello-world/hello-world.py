46bda71a4c79bda279b2fe6508df99af4bcdbfc7
Lawrence <lawrence.zhou2010@gmail.com>
2025-07-28 15:53:26 -0400

    Update hello-world.py

--- hello-world/hello-world.py
+++ hello-world/hello-world.py
@@ -11,11 +11,17 @@ def hello():
 
 # Activity 4.2: Use routes, using routes to edit format
 
+@app.route("/user/<name>")
+def user(name):
+    return f"<h1>Hello, {name}!</h1>"
 
 # Activity 4.3 Introduce --debug to allow refresh on change 
 
 # Activity 4.4: rendering HTML templates
 
+@app.route("/time/<name>")
+def render_file(name):
+    return render_template("index.html", name=name, current_time=datetime.now())
 
 # Activity 4.5: 
 
